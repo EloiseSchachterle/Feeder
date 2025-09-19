b4b7e9de7fcce8995076466a437bbe377454e81b
EloiseSchachterle <eloises3@nycstudents.net>
2025-09-19 15:05:21 -0400

    Feeder

--- Main.java
+++ Main.java
@@ -4,8 +4,13 @@ public class Main
     {
         Feeder f = new Feeder(500);
         System.out.println(f);
-        System.out.println(f.getCurrentFood());
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f.simulateOneDay(12);
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(1000);
         f.simulateOneDay(22);
-        System.out.println(f.getCurrentFood());
+        System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(0);
+        System.out.println(f.simulateManyDays(10,4) + " days");
     }
 }
